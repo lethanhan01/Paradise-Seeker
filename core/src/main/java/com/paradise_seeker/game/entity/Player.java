4d555a68fe5e3cd17ac6b35cd13d8e097e5fabad
hiepNH272 <hiep.nh235708@sis.hust.edu.vn>
2025-06-04 08:35:35 +0700

    Fix player's move

--- core/src/main/java/com/paradise_seeker/game/entity/Player.java
+++ core/src/main/java/com/paradise_seeker/game/entity/Player.java
@@ -167,7 +167,8 @@ public class Player extends Character {
 
         hp = Math.max(0, hp - damage);
 
-        if (hp == 0 && !isDead) {
+        if (hp == 0 ) {
+        	if (!isDead) 
             onDeath();
         } else {
             isHit = true;
