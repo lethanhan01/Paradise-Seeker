391bb4943b81a745e59a5dbbfe1e3ba1d36f2c9d
lethanhan01 <An.LT235631@sis.hust.edu.vn>
2025-06-10 18:44:42 +0700

    helo

--- core/src/main/java/com/paradise_seeker/game/entity/skill/Skill.java
+++ core/src/main/java/com/paradise_seeker/game/entity/skill/Skill.java
@@ -5,12 +5,7 @@ import com.badlogic.gdx.math.Rectangle;
 
 public interface Skill {
     boolean canUse(long now);
-    void execute(Character user);
     void update(long now);
-    void castSkill(float atk, Character target);
-    void castSkill(float atk, float x, float y);  // Để bắn ở vị trí x, y
     void castSkill(float atk, float x, float y, String direction);  // Bắn theo hướng
     void castSkill(float atk, Rectangle playerBounds, String direction); // Bắn theo hướng từ player
-
-    float getManaCost();
 }
