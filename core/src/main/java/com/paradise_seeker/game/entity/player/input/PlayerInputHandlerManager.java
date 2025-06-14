7212a62d75744560dd6827631f71e9f1af4c3b00
hiepNH272 <hiep.nh235708@sis.hust.edu.vn>
2025-06-14 23:02:54 +0700

    Sửa lỗi scale font Winscr

--- core/src/main/java/com/paradise_seeker/game/entity/player/input/PlayerInputHandlerManager.java
+++ core/src/main/java/com/paradise_seeker/game/entity/player/input/PlayerInputHandlerManager.java
@@ -323,10 +323,11 @@ public class PlayerInputHandlerManager implements PlayerInputHandler {
         }
         if (gameScreen.currentTalkingNPC != null) {
         	gameScreen.currentTalkingNPC.setTalking(false);
+        	
         }
         this.showDialogueOptions = false;
         this.selectedOptionIndex = 0;
-        gameScreen.currentTalkingNPC.stateManager.isChestOpened = false;
+        gameScreen.currentTalkingNPC.stateManager.isChestOpened = false;     
     }
 
 
