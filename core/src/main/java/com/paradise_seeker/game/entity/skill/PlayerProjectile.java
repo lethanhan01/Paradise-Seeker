0bfb4fb6640a1c4a12108f4cee6fa7cf195a7095
nhithedev <nhitofu@gmail.com>
2025-06-14 16:04:34 +0700

    fixed render

--- core/src/main/java/com/paradise_seeker/game/entity/skill/PlayerProjectile.java
+++ core/src/main/java/com/paradise_seeker/game/entity/skill/PlayerProjectile.java
@@ -2,10 +2,11 @@ package com.paradise_seeker.game.entity.skill;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.paradise_seeker.game.rendering.Renderable;
 
-public interface PlayerProjectile {
+public interface PlayerProjectile extends Renderable {
     void update();
-    void isRendered(SpriteBatch batch);
+    void render(SpriteBatch batch);
     boolean isActive();
     Rectangle getHitbox();
     float getDamage();
